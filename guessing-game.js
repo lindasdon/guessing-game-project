c1aba541f49a4c1c1b6cdf77909415997a8a6eff
Donald Woodhouse <lindas76don@gmail.com>
2024-04-08 23:15:05 -0400

    second commit with bonus

--- guessing-game.js
+++ guessing-game.js
@@ -4,49 +4,64 @@ const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
-
-let randomInRange = (min, max) => {
-	return min + Math.floor(Math.random() * (max + 1 - min));
-}
-
-let secretNumber;
-
-let checkGuess = guess => {
-	if (guess > secretNumber) {
-		console.log('Too high.');
-		return false;
+if (true) {
+	let limit = 0;
+	let randomInRange = (min, max) => {
+		return min + Math.floor(Math.random() * (max + 1 - min));
 	}
-	if (guess < secretNumber) {
-		console.log('Too low.');
-		return false;
-	}
-	if (guess === secretNumber) {
-		console.log('Correct!');
-		return true;
-	}
-}
 
-let askGuess = () => {
-	rl.question('Enter a guess:', guess => {
-		if(checkGuess(Number(guess))) {
-			rl.close();
-		} else {
-			askGuess();
+	let secretNumber;
+
+	let checkGuess = guess => {
+		if (guess > secretNumber) {
+			console.log('Too high.');
+			return false;
 		}
-	});
-}
+		if (guess < secretNumber) {
+			console.log('Too low.');
+			return false;
+		}
+		if (guess === secretNumber) {
+			console.log('Correct!');
+			return true;
+		}
+	}
 
-let askRange = () => {
-	rl.question('Enter a min number:', minStr => {
-		rl.question('Enter a max number:', maxStr => {
-			let min = Number(minStr); let max = Number(maxStr);
-			if(Number(max) >= Number(min)) {
-				console.log("I'm thinking of a number between " + min + " and " + max + "...");
-				secretNumber = randomInRange(min, max);
-				askGuess();
+	let askGuess = () => {
+		rl.question('Enter a guess:', guess => {
+			if(checkGuess(Number(guess))) {
+				rl.close();
+			} else {
+				limit--;
+				if (limit === 0) {
+					console.log('You lose!');
+					rl.close();
+				} else {
+					askGuess();
+				}
 			}
 		});
-	});
-}
+	}
+
+	let askLimit = () => {
+		rl.question('How many tries do you need?', num => {
+			limit = Number(num);
+			askGuess();
+		});
+	}
+
+	let askRange = () => {
+		rl.question('Enter a min number:', minStr => {
+			rl.question('Enter a max number:', maxStr => {
+				let min = Number(minStr); let max = Number(maxStr);
+				if(Number(max) >= Number(min)) {
+					console.log("I'm thinking of a number between " + min + " and " + max + "...");
+					secretNumber = randomInRange(min, max);
+					askLimit();
+				}
+			});
+		});
+	}
 
 askRange();
+}
